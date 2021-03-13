b6170ed50a7923c2ab6028a9353215bc955ea825
zcy <514834905@qq.com>
2021-03-13 21:19:18 +0800

    完成了主页功能的开发

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function() {
+        //获取用户基本信息
+        getUserInfo();
+        //给退出绑定点击事件
+        $("#loginout").on("click", function() {
+            layui.layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+                //do something
+                localStorage.removeItem("token")
+                location.href = "/login.html"
+                layer.close(index);
+            });
+        })
+    })
+    //获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: "GET",
+        url: "/my/userinfo",
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            //调动renderAvater渲染用户头像
+            renderAvater(res.data)
+        }
+    })
+}
+
+function renderAvater(user) {
+    //获取用户名称
+    var name = user.nickname || user.username;
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name)
+        //获取头像
+    if (user.user_pic != null) {
+        $(".layui-nav-img").attr("src", user.user_pic).show()
+        $(".text-avatar").hide();
+    } else {
+        var first = name[0].toUpperCase();
+        $(".text-avatar").html(first).show()
+        $(".layui-nav-img").hide();
+    }
+
+}
